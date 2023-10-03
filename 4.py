97fdfecf4996f7e5742e0fb1e2669213fcf88e5d
Korneychenko <146822409+Korneychenko@users.noreply.github.com>
2023-10-03 12:53:20 +0300

    Delete 4.py

--- 4.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-second=float(input("Введите колличество секунд: "))
-minuts=float(second / 60)
-hours=float(second / 3600)
-days=float(second / 86400)
-print(f"Минуты: {'%.2f'% minuts}\nЧасы: {'%.4f'% hours}\nДни: {'%.5f'% days}")
